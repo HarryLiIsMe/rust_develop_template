5d708c28e638c5d92ae05cec05e0064b0224d575
HarryLiIsMe <42673017+HarryLiIsMe@users.noreply.github.com>
2025-01-04 23:27:34 +0800

    Update main.rs

--- src/rust_develop_template1/main.rs
+++ src/rust_develop_template1/main.rs
@@ -4,7 +4,7 @@ use std::env;
 fn main() {
     dotenv().ok();
 
-    let project_name = env::var("PROJECT_NAME").unwrap();
+    let project_name = env::var("PROJECT_NAME").expect("env load failed!!!");
 
     println!("hello world1 {}", project_name);
 }
